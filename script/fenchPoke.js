8223df73f01d242e2adb5c4e20d2f1562fd7f11a
Samuelllr <32439511+samuelllr@users.noreply.github.com>
2021-06-30 02:15:17 -0300

    Update fenchPoke.js
    busca de dados e teste de imprimir no console.

--- script/fenchPoke.js
+++ script/fenchPoke.js
@@ -1,8 +1,31 @@
 // código de busca de dados em formato JSON
 
+//VAR select id
+var idPoke = 25; //pikachu 25
+
 const fechPokemon = () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/25'; //pikachu 25
-    fetch( url ).then( response => response.json()).then( pokemon => { console.log(pokemon) } );
+
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + idPoke ; 
+    fetch( url ).then( response => response.json()).then( pokemon => { 
+        
+        console.log(pokemon); 
+        
+        //pegando dados da PokeAPI
+
+            //nome do pokemon
+        var namePoke = pokemon.name;
+        console.log( "nome: " + namePoke );
+
+            //abilidade 1 do pokemon
+        var ability1 = pokemon.abilities[0].ability.name;
+        console.log( "Abilidade 1: " + ability1 );
+
+            //abilidade 2 do pokemon
+         var ability2 = pokemon.abilities[1].ability.name;
+         console.log( "Abilidade 1: " + ability2 );
+
+    } );
+
 }
 
 fechPokemon();
