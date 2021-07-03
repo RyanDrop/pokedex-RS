859cc490c48e5988b9c9cbc308442f17916b5377
Samuelllr <32439511+samuelllr@users.noreply.github.com>
2021-07-03 02:59:09 -0300

    Tela Load

--- script/animation.js
+++ script/animation.js
@@ -1,1 +1,19 @@
 // file to animation app/web
+
+//VAR's
+
+var div_load = document.getElementById("div-load");
+var img_load1 = document.getElementById("img-load1");
+var img_load2 = document.getElementById("img-load2");
+
+function trocarGifLoad(){
+    img_load1.style.display = "none";
+}
+
+window.setTimeout( "trocarGifLoad()" , 6300 );
+
+function trocarGifLoad2(){
+    div_load.style.display = "none";
+}
+
+window.setTimeout( "trocarGifLoad2()" , 7300 );
