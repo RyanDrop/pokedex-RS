a85f02b1fd05b07afdf9d79b0c4f9fd07f83442b
Samuelllr <32439511+samuelllr@users.noreply.github.com>
2021-07-06 02:13:25 -0300

    Adicionado funçoes no bar menu.
    info.html excluído , animações adicionadas, div info adicionado. modo noturno aprimorado.

--- script/animation.js
+++ script/animation.js
@@ -1,11 +1,13 @@
 // file to animation app/web
 
 //VAR's
-var div_load = document.getElementById("div-load");
-var img_load = document.getElementById("img-load");
 //------------------------------------------------
 
 // ANIMATE LOAD
+
+var div_load = document.getElementById("div-load");
+var img_load = document.getElementById("img-load");
+
 window.setTimeout( function(){
     img_load.src = "img/loading-gifs/pikachu.gif";
 }, 6900 );
@@ -28,5 +30,30 @@ for(var i=0; i<btn.length; i++){
         current[0].className = current[0].className.replace('active');
         this.className += ' active';
     })
+}
+//------------------------------------------------
+
+//ANIMATION MENU
+var infoDiv = document.getElementById("info");
+var modeNoturn = 0;
+
+function activeMenu( mode ){
+    switch( mode ){
+        case 1: 
+            if( modeNoturn == 0 ){
+                document.body.style.backgroundColor = "#222";
+                infoDiv.style.backgroundColor = "#222";
+                modeNoturn = 1 ;
+            }else{
+                document.body.style.backgroundColor = "#fff";
+                infoDiv.style.backgroundColor = "#fff";
+                modeNoturn = 0 ;
+            }
+        break;
+        case 2:
+            infoDiv.style.top = "100%" ;
+        break;
+        case 3: infoDiv.style.top = "30vh"; break;
+    }
 }
 //------------------------------------------------
