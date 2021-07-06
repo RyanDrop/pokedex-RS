9edc2788ee34c8e00bdfd3c67bf635e587e89059
Samuelllr <32439511+samuelllr@users.noreply.github.com>
2021-07-06 19:46:07 -0300

    Organização de arquivos

---
+++ MainFiles/script/engine.js
@@ -0,0 +1,8 @@
+// Motor
+
+//Teste img API contener
+var foto = document.getElementById("foto");
+var name = document.getElementById("pokeName");
+name.innerHTML = pokeName ;
+console.log("engine name: " + pokeName );
+foto.src = "https://pokeres.bastionbot.org/images/pokemon/" + idPoke + ".png";
--- script/engine.js
+++
@@ -1,8 +0,0 @@
-// Motor
-
-//Teste img API contener
-var foto = document.getElementById("foto");
-var name = document.getElementById("pokeName");
-name.innerHTML = pokeName ;
-console.log("engine name: " + pokeName );
-foto.src = "https://pokeres.bastionbot.org/images/pokemon/" + idPoke + ".png";
