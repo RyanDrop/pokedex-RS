02ccdeea3544f96868bfa7a7213f02477898da6d
Ryan-GitHub7 <ryangithub7509@gmail.com>
2021-07-06 15:32:48 -0300

    DarkMode + Info
    Dark mode através de root e ajustei as informações

--- script/animation.js
+++ script/animation.js
@@ -26,21 +26,21 @@ var modeNoturn = 0;
 
 function activeMenu( mode ){
     switch( mode ){
-        case 1: 
-            if( modeNoturn == 0 ){
-                document.body.style.backgroundColor = "#222";
-                infoDiv.style.backgroundColor = "#222";
-                modeNoturn = 1 ;
-            }else{
-                document.body.style.backgroundColor = "#fff";
-                infoDiv.style.backgroundColor = "#fff";
-                modeNoturn = 0 ;
-            }
-        break;
+        //case 1: 
+            //if( modeNoturn == 0 ){
+                //document.body.style.backgroundColor = "#222";
+               // infoDiv.style.backgroundColor = "#222";
+               // modeNoturn = 1 ;
+            //}else{
+               // document.body.style.backgroundColor = "#fff";
+               // infoDiv.style.backgroundColor = "#fff";
+                //modeNoturn = 0 ;
+           // }
+        //break;
         case 2:
             infoDiv.style.top = "100%" ;
         break;
-        case 3: infoDiv.style.top = "30vh"; break;
+        case 3: infoDiv.style.top = "0vh"; break;
     }
 }
 //------------------------------------------------
