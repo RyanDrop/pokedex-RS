6efb3907b6d61d574f55315f41ba44c63e5f26b5
Ryan-GitHub7 <ryangithub7509@gmail.com>
2021-07-09 14:48:24 -0300

    Container principal

--- MainFiles/script/animation.js
+++ MainFiles/script/animation.js
@@ -57,7 +57,6 @@ function pokeinfoF( id ){
     pokeinfo.style.top = "0rem" ;
     pokeinfo.style.backgroundColor = colors[ id-1 ]; 
     var idimg = document.getElementById( "pokeImg"+id );
-    idimg.style = "z-index:4;width:auto;height:30vh;position:fixed;top:40%;left:50%;transform: translate(-40%,-50%);" ;
+    idimg.style = "z-index:4;width:auto;height:30vh;position:fixed;top:37%;left:50%;transform: translate(-37%,-50%);" ;
     armazemIDimg.push( "pokeImg" + id );
-    console.log[ armazemIDimg ];
 }
