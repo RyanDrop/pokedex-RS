a4f99e32c38e029de0284a4c39c27dfc0c36217c
Samuelllr <32439511+samuelllr@users.noreply.github.com>
2021-07-11 02:29:25 -0300

    Canvas Particles adicionado , melhoria de designer , bug corrigido

--- MainFiles/script/animation.js
+++ MainFiles/script/animation.js
@@ -2,6 +2,8 @@
 
 //VAR's
 var pokeinfo = document.getElementById("pokeinfo");
+var sobre = document.getElementById("sobre");
+var stats = document.getElementById("stats");
 var armazemIDimg = [];
 //------------------------------------------------
 
@@ -56,6 +58,8 @@ function activeMenu( mode ){
 function pokeinfoF( id ){ 
     pokeinfo.style.top = "0rem" ;
     pokeinfo.style.backgroundColor = colors[ id-1 ]; 
+    sobre.style.color = colors[ id-1 ] ;
+    stats.style.color = colors[ id-1 ] ;
     var idimg = document.getElementById( "pokeImg"+id );
     idimg.style = "z-index:4;width:auto;height:33vh;position:fixed;top:40%;left:47%;transform: translate(-40%,-47%);" ;
     armazemIDimg.push( "pokeImg" + id );
@@ -66,7 +70,120 @@ const range = document.querySelector('#range'),
 	progressbar = document.querySelector('.progress-bar1');
   
   
-range.addEventListener('input', function(){
+document.addEventListener('input', function(){
 	const value = range.value;
   progressbar.style.setProperty('Hp', value)
-})
+});
+
+//PARTICLES JS
+
+particlesJS("particles-js",{
+    "particles": {
+        "number": {
+          "value": 50,
+          "density": {
+            "enable": false,
+            "value_area": 500
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.8,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 5,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 10,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 300,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 2
+        },
+        "move": {
+          "enable": true,
+          "speed": 1,
+          "direction": "top",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 800,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 800,
+            "size": 80,
+            "duration": 2,
+            "opacity": 0.8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 400,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+});
