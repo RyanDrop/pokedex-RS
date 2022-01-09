7ffe4456b0766159b69b8947b16c2cad4193c73f
RyanDrop <ryangithub7509@gmail.com>
2022-01-09 13:01:12 -0300

    📦 refactor: API link update

--- MainFiles/script/animation.js
+++ MainFiles/script/animation.js
@@ -13,186 +13,185 @@ var armazemIDimg = [];
 var div_load = document.getElementById("div-load");
 var img_load = document.getElementById("img-load");
 
-window.setTimeout( function(){
-    img_load.src = "img/loading-gifs/pikachu.gif";
-}, 6900 );
+window.setTimeout(function () {
+  img_load.src = "MainFiles/img/loading-gifs/pikachu.gif";
+}, 6900);
 
-window.setTimeout( function(){
-    div_load.style.transform = "scale(100)";
-    window.setTimeout(function(){
-        div_load.style.display = "none";
-        document.body.style.overflowY = "visible";
-    },100);
-}, 8900 );
+window.setTimeout(function () {
+  div_load.style.transform = "scale(100)";
+  window.setTimeout(function () {
+    div_load.style.display = "none";
+    document.body.style.overflowY = "visible";
+  }, 100);
+}, 8900);
 //------------------------------------------------
 
 //ANIMATION MENU
 var infoDiv = document.getElementById("info");
 var modeNoturn = 0;
 
-function activeMenu( mode ){
-    switch( mode ){
-        case 1:
-            infoDiv.style.top = "100%" ;
-            pokeinfo.style.top = "100%";
+function activeMenu(mode) {
+  switch (mode) {
+    case 1:
+      infoDiv.style.top = "100%";
+      pokeinfo.style.top = "100%";
 
-            var idimg = document.getElementById( armazemIDimg[0] );
-            idimg.style = "z-index:1" ;
-            armazemIDimg.shift();
+      var idimg = document.getElementById(armazemIDimg[0]);
+      idimg.style = "z-index:1";
+      armazemIDimg.shift();
 
-        break;
-        case 2: 
-            infoDiv.style.top = "0vh"; 
-            pokeinfo.style.top = "100%";
+      break;
+    case 2:
+      infoDiv.style.top = "0vh";
+      pokeinfo.style.top = "100%";
 
-            var idimg = document.getElementById( armazemIDimg[0] );
-            idimg.style = "z-index:1" ;
-            armazemIDimg.shift();
-         
-        break;
-    }
+      var idimg = document.getElementById(armazemIDimg[0]);
+      idimg.style = "z-index:1";
+      armazemIDimg.shift();
+
+      break;
+  }
 }
 //------------------------------------------------
 
 //DIV INFO POKEINFO IMG
 
-function pokeinfoF( id ){ 
-    armazemIDimg.shift();
-    pokeinfo.style.top = "0rem" ;
-    pokeinfo.style.backgroundColor = colors[ id-1 ]; 
-    sobre.style.color = colors[ id-1 ] ;
-    stats.style.color = colors[ id-1 ] ;
-    var idimg = document.getElementById( "pokeImg"+id );
-    idimg.style = "z-index:4;width:auto;height:33vh;position:fixed;top:40%;left:47%;transform: translate(-40%,-47%);" ;
-    armazemIDimg.push( "pokeImg" + id );
+function pokeinfoF(id) {
+  armazemIDimg.shift();
+  pokeinfo.style.top = "0rem";
+  pokeinfo.style.backgroundColor = colors[id - 1];
+  sobre.style.color = colors[id - 1];
+  stats.style.color = colors[id - 1];
+  var idimg = document.getElementById("pokeImg" + id);
+  idimg.style = "z-index:4;width:auto;height:33vh;position:fixed;top:40%;left:47%;transform: translate(-40%,-47%);";
+  armazemIDimg.push("pokeImg" + id);
 
-    //CHAMADA DE MOTOR PARA BUSCA DE DADOS
-        engine( 1 , id );
-    //------------------------------------
+  //CHAMADA DE MOTOR PARA BUSCA DE DADOS
+  engine(1, id);
+  //------------------------------------
 }
 
 //PARTICLES JS
 
-particlesJS("particles-js",{
-    "particles": {
-        "number": {
-          "value": 50,
-          "density": {
-            "enable": false,
-            "value_area": 500
-          }
-        },
-        "color": {
-          "value": "#ffffff"
-        },
-        "shape": {
-          "type": "circle",
-          "stroke": {
-            "width": 0,
-            "color": "#000000"
-          },
-          "polygon": {
-            "nb_sides": 5
-          },
-          "image": {
-            "src": "img/github.svg",
-            "width": 100,
-            "height": 100
-          }
-        },
-        "opacity": {
-          "value": 0.8,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 5,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 10,
-            "size_min": 0.1,
-            "sync": false
-          }
-        },
-        "line_linked": {
-          "enable": false,
-          "distance": 300,
-          "color": "#ffffff",
-          "opacity": 0.4,
-          "width": 2
-        },
-        "move": {
-          "enable": true,
-          "speed": 1,
-          "direction": "top",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-          }
-        }
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: false,
+        value_area: 500,
       },
-      "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-          "onhover": {
-            "enable": false,
-            "mode": "repulse"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "push"
-          },
-          "resize": true
-        },
-        "modes": {
-          "grab": {
-            "distance": 800,
-            "line_linked": {
-              "opacity": 1
-            }
-          },
-          "bubble": {
-            "distance": 800,
-            "size": 80,
-            "duration": 2,
-            "opacity": 0.8,
-            "speed": 3
-          },
-          "repulse": {
-            "distance": 400,
-            "duration": 0.4
-          },
-          "push": {
-            "particles_nb": 4
-          },
-          "remove": {
-            "particles_nb": 2
-          }
-        }
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
       },
-      "retina_detect": true
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.8,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 300,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "top",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 800,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 800,
+        size: 80,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
 });
 
 // DARK MODE
 var mode = 0;
-function dark(){
+function dark() {
   mode++;
-  if( mode == 1 ){
+  if (mode == 1) {
     body.style.backgroundColor = "rgb(36,36,36)";
-  }else{ 
+  } else {
     mode = 0;
-    body.style.backgroundColor = "#fff"; 
+    body.style.backgroundColor = "#fff";
   }
-
 }
--- MainFiles/script/engine.js
+++ MainFiles/script/engine.js
@@ -22,206 +22,205 @@ var hab1 = document.getElementById("hab1");
 var hab2 = document.getElementById("hab2");
 var descricaoo = document.getElementById("descricao");
 
-function engine( mode , idPoke , color ){
+function engine(mode, idPoke, color) {
+  switch (mode) {
+    case 0:
+      var url = "https://pokeapi.co/api/v2/pokemon/" + idPoke;
+      fetch(url)
+        .then((response) => response.json())
+        .then((pokemon) => {
+          var container = document.createElement("div");
+          var clase2 = document.createAttribute("class");
+          var containerStyle = document.createAttribute("style");
+          var onTouch = document.createAttribute("onclick");
+          var idcontainer = document.createAttribute("id");
+          clase2.value = "container";
+          onTouch.value = "pokeinfoF(" + idPoke + ")";
+          idcontainer.value = "poke" + idPoke;
 
-    switch( mode ){
+          if (color == null || undefined || "") {
+            color = "#555";
+          }
+          containerStyle.value = "background-color:" + color + ";";
 
-        case 0:
+          container.setAttributeNode(clase2);
+          container.setAttributeNode(onTouch);
+          container.setAttributeNode(idcontainer);
+          container.setAttributeNode(containerStyle);
 
-            var url = 'https://pokeapi.co/api/v2/pokemon/' + idPoke ; 
-            fetch( url ).then( response => response.json()).then( pokemon => { 
+          var h4 = document.createElement("h4");
+          var number = document.createElement("p");
+          var br = document.createElement("br");
+          var p = document.createElement("p");
+          var ptype = document.createElement("p");
 
-                var container = document.createElement("div");
-                var clase2 = document.createAttribute("class");
-                var containerStyle = document.createAttribute("style");
-                var onTouch = document.createAttribute("onclick");
-                var idcontainer = document.createAttribute("id");
-                clase2.value = "container";
-                onTouch.value = "pokeinfoF("+idPoke+")";
-                idcontainer.value = "poke" + idPoke ;
-                
-                if( color == null||undefined||"" ){ color = "#555" ; }
-                containerStyle.value = "background-color:" + color + ";" ;
-                
-                container.setAttributeNode( clase2 );
-                container.setAttributeNode( onTouch );
-                container.setAttributeNode( idcontainer );
-                container.setAttributeNode( containerStyle );
+          var img = document.createElement("img");
+          var imgSrc = document.createAttribute("src");
+          var idimgpoke = document.createAttribute("id");
+          idimgpoke.value = "pokeImg" + idPoke;
+          imgSrc.value = "https://cdn.traction.one/pokedex/pokemon/" + idPoke + ".png";
 
-                var h4  = document.createElement("h4");
-                var number = document.createElement("p");
-                var br = document.createElement("br");
-                var p = document.createElement("p");
-                var ptype = document.createElement("p"); 
+          img.setAttributeNode(idimgpoke);
+          img.setAttributeNode(imgSrc);
 
-                var img = document.createElement("img");
-                var imgSrc = document.createAttribute("src");
-                var idimgpoke = document.createAttribute("id");
-                idimgpoke.value = "pokeImg"+idPoke ;
-                imgSrc.value = "https://pokeres.bastionbot.org/images/pokemon/" + idPoke + ".png";
-                
-                img.setAttributeNode( idimgpoke );
-                img.setAttributeNode( imgSrc );
+          var classpp1 = document.createAttribute("class");
+          var classpp2 = document.createAttribute("class");
+          var classpp3 = document.createAttribute("class");
+          classpp1.value = "pp";
+          classpp2.value = "pp";
+          classpp3.value = "number";
+          p.setAttributeNode(classpp1);
+          ptype.setAttributeNode(classpp2);
+          number.setAttributeNode(classpp3);
 
-                var classpp1 = document.createAttribute("class");
-                var classpp2 = document.createAttribute("class");
-                var classpp3 = document.createAttribute("class");
-                classpp1.value = "pp";
-                classpp2.value = "pp";
-                classpp3.value = "number";
-                p.setAttributeNode( classpp1 );
-                ptype.setAttributeNode( classpp2 );
-                number.setAttributeNode( classpp3 );
+          h4.innerHTML = pokemon.name;
+          p.innerHTML = pokemon.types[0].type.name;
 
-                h4.innerHTML = pokemon.name;
-                p.innerHTML = pokemon.types[0].type.name;
+          if (idPoke < 10) {
+            number.innerHTML = "#00" + idPoke;
+          }
+          if (idPoke >= 10 && idPoke < 100) {
+            number.innerHTML = "#0" + idPoke;
+          }
+          if (idPoke >= 100) {
+            number.innerHTML = "#" + idPoke;
+          }
 
-                if( idPoke < 10 ){
-                    number.innerHTML = "#00" + idPoke ;
-                }if( ( idPoke >= 10 )&&( idPoke < 100 ) ){
-                    number.innerHTML = "#0" + idPoke ;
-                }if( idPoke >= 100 ){
-                    number.innerHTML = "#" + idPoke ;
-                }
-                
-                if( pokemon.types[1] != undefined||null ){
-                    ptype.innerHTML = pokemon.types[1].type.name;
-                }else{
-                    ptype.style.display = "none" ;
-                    img.style.marginTop = "15px";
-                }
+          if (pokemon.types[1] != undefined || null) {
+            ptype.innerHTML = pokemon.types[1].type.name;
+          } else {
+            ptype.style.display = "none";
+            img.style.marginTop = "15px";
+          }
 
-                container.appendChild( h4 );
-                container.appendChild( number );
-                container.appendChild( p );
-                container.appendChild( br );
-                container.appendChild( ptype );
-                container.appendChild( img );
+          container.appendChild(h4);
+          container.appendChild(number);
+          container.appendChild(p);
+          container.appendChild(br);
+          container.appendChild(ptype);
+          container.appendChild(img);
 
-                containers.appendChild( container );
+          containers.appendChild(container);
 
-                nav.appendChild( containers );
-                
-            });
-        
-        break;
-        case 1:
+          nav.appendChild(containers);
+        });
 
-            var url = 'https://pokeapi.co/api/v2/pokemon/' + idPoke ; 
-            fetch( url ).then( response => response.json()).then( pokemon => { 
-                  
-                //nome do pokemon
-                var namePoke = pokemon.name;
-                namePokeInfo.innerHTML = namePoke;
+      break;
+    case 1:
+      var url = "https://pokeapi.co/api/v2/pokemon/" + idPoke;
+      fetch(url)
+        .then((response) => response.json())
+        .then((pokemon) => {
+          //nome do pokemon
+          var namePoke = pokemon.name;
+          namePokeInfo.innerHTML = namePoke;
 
-                var descricao = pokemon.species.url;
-                fetch( descricao ).then( response => response.json()).then( descrption => {
-
-                    var descp1 = descrption.flavor_text_entries[10].flavor_text;
-                    descricaoo.innerHTML = descp1 ;
-
-                } );
-
-                //TIPO 1
-                var typePoke1 = pokemon.types[0].type.name;
-                typePokeinfo1.innerHTML = typePoke1;
-                tipoPoke.innerHTML = typePoke1;
-
-                //TIPO 2
-                if( pokemon.types[1] != undefined||null ){
-                    typePokeinfo2.style.display = "inline" ;
-                    var typePoke2 = pokemon.types[1].type.name;
-                    typePokeinfo2.innerHTML = typePoke2;
-                }else{
-                    typePokeinfo2.style.display = "none" ;
-                }
-
-                //Number Poke
-                if( idPoke < 10 ){
-                    numberPoke.innerHTML = "#00" + idPoke ;
-                }if( ( idPoke >= 10 )&&( idPoke < 100 ) ){
-                    numberPoke.innerHTML = "#0" + idPoke ;
-                }if( idPoke >= 100 ){
-                    numberPoke.innerHTML = "#" + idPoke ;
-                }
-
-                //altura do pokemon
-                var heightPoke = pokemon.height;
-                var stringHeight = heightPoke + "" ;
-                if( stringHeight[1] == undefined||null ){
-                    var stringHeightPronta = "0," + stringHeight[0] + "m" ;
-                    alturaPoke.innerHTML = stringHeightPronta;
-                }else{
-                    var stringHeightPronta = stringHeight[0] + "," + stringHeight[1] + "m";
-                    alturaPoke.innerHTML = stringHeightPronta;
-                }
-
-                //Peso
-                var weightPoke = pokemon.weight;
-                var string = weightPoke + "" ;
-                if( string[2] != undefined||null ){
-                    var stringPronta = string[0] + string[1] + "," + string[2] + "Kg";
-                    pesoPoke.innerHTML = stringPronta ;
-                }
-                if( string[2] == undefined||null ){
-                    var stringPronta = string[0] + "," + string[1] + "Kg";
-                    pesoPoke.innerHTML = stringPronta ;
-                }
-                if( ( string[1] == undefined||null )&&( string[2] == undefined||null ) ){
-                    var stringPronta = "0," + string[0] + "Kg";
-                    pesoPoke.innerHTML = stringPronta ;
-                }
-                if( string[3] != undefined||null ){
-                    var stringPronta = string[0] + string[1] + string[2] + "," + string[3] + "Kg";
-                    pesoPoke.innerHTML = stringPronta ;
-                }
-
-
-                //HP
-                var hpPoke = pokemon.stats[0].base_stat;
-                barHP.style.setProperty("--Hp", hpPoke/1.5 );
-                barHP.innerHTML = hpPoke;
-
-                //Ataque
-                var atackPoke = pokemon.stats[1].base_stat;
-                barAtk.style.setProperty("--Atk", atackPoke/1.5 );
-                barAtk.innerHTML = atackPoke;
-
-                //Defesa
-                var defesaPoke = pokemon.stats[2].base_stat;
-                barDef.style.setProperty("--Def", defesaPoke/1.5 );
-                barDef.innerHTML = defesaPoke;
-
-                //Especial-atack
-                var espAtackPoke = pokemon.stats[3].base_stat;
-                barEspAtk.style.setProperty("--OverAtk", espAtackPoke/1.5 );
-                barEspAtk.innerHTML = espAtackPoke;
-
-                //Especial-defesa
-                var espDefesaPoke = pokemon.stats[4].base_stat;
-                barEspDef.style.setProperty("--Overdef" , espDefesaPoke/1.5 );
-                barEspDef.innerHTML = espDefesaPoke;
-
-                //velocidade
-                var veloPoke = pokemon.stats[5].base_stat;
-                barSpeed.style.setProperty("--Speed" , veloPoke/1.5 );
-                barSpeed.innerHTML = veloPoke ;
-
-                //habilidades
-                var habilidade1 = pokemon.abilities[0].ability.name;
-                hab1.innerHTML = habilidade1;
-                
-                if( pokemon.abilities[1] != undefined||null ){
-                    var habilidade2 = pokemon.abilities[1].ability.name;
-                    hab2.innerHTML = habilidade2 ;
-                }else{
-                    hab2.innerHTML = "none" ;
-                }
-            
+          var descricao = pokemon.species.url;
+          fetch(descricao)
+            .then((response) => response.json())
+            .then((descrption) => {
+              var descp1 = descrption.flavor_text_entries[10].flavor_text;
+              descricaoo.innerHTML = descp1;
             });
 
-        break;
-    
-    }
+          //TIPO 1
+          var typePoke1 = pokemon.types[0].type.name;
+          typePokeinfo1.innerHTML = typePoke1;
+          tipoPoke.innerHTML = typePoke1;
 
+          //TIPO 2
+          if (pokemon.types[1] != undefined || null) {
+            typePokeinfo2.style.display = "inline";
+            var typePoke2 = pokemon.types[1].type.name;
+            typePokeinfo2.innerHTML = typePoke2;
+          } else {
+            typePokeinfo2.style.display = "none";
+          }
+
+          //Number Poke
+          if (idPoke < 10) {
+            numberPoke.innerHTML = "#00" + idPoke;
+          }
+          if (idPoke >= 10 && idPoke < 100) {
+            numberPoke.innerHTML = "#0" + idPoke;
+          }
+          if (idPoke >= 100) {
+            numberPoke.innerHTML = "#" + idPoke;
+          }
+
+          //altura do pokemon
+          var heightPoke = pokemon.height;
+          var stringHeight = heightPoke + "";
+          if (stringHeight[1] == undefined || null) {
+            var stringHeightPronta = "0," + stringHeight[0] + "m";
+            alturaPoke.innerHTML = stringHeightPronta;
+          } else {
+            var stringHeightPronta = stringHeight[0] + "," + stringHeight[1] + "m";
+            alturaPoke.innerHTML = stringHeightPronta;
+          }
+
+          //Peso
+          var weightPoke = pokemon.weight;
+          var string = weightPoke + "";
+          if (string[2] != undefined || null) {
+            var stringPronta = string[0] + string[1] + "," + string[2] + "Kg";
+            pesoPoke.innerHTML = stringPronta;
+          }
+          if (string[2] == undefined || null) {
+            var stringPronta = string[0] + "," + string[1] + "Kg";
+            pesoPoke.innerHTML = stringPronta;
+          }
+          if ((string[1] == undefined || null) && (string[2] == undefined || null)) {
+            var stringPronta = "0," + string[0] + "Kg";
+            pesoPoke.innerHTML = stringPronta;
+          }
+          if (string[3] != undefined || null) {
+            var stringPronta = string[0] + string[1] + string[2] + "," + string[3] + "Kg";
+            pesoPoke.innerHTML = stringPronta;
+          }
+
+          //HP
+          var hpPoke = pokemon.stats[0].base_stat;
+          barHP.style.setProperty("--Hp", hpPoke / 1.5);
+          barHP.innerHTML = hpPoke;
+
+          //Ataque
+          var atackPoke = pokemon.stats[1].base_stat;
+          barAtk.style.setProperty("--Atk", atackPoke / 1.5);
+          barAtk.innerHTML = atackPoke;
+
+          //Defesa
+          var defesaPoke = pokemon.stats[2].base_stat;
+          barDef.style.setProperty("--Def", defesaPoke / 1.5);
+          barDef.innerHTML = defesaPoke;
+
+          //Especial-atack
+          var espAtackPoke = pokemon.stats[3].base_stat;
+          barEspAtk.style.setProperty("--OverAtk", espAtackPoke / 1.5);
+          barEspAtk.innerHTML = espAtackPoke;
+
+          //Especial-defesa
+          var espDefesaPoke = pokemon.stats[4].base_stat;
+          barEspDef.style.setProperty("--Overdef", espDefesaPoke / 1.5);
+          barEspDef.innerHTML = espDefesaPoke;
+
+          //velocidade
+          var veloPoke = pokemon.stats[5].base_stat;
+          barSpeed.style.setProperty("--Speed", veloPoke / 1.5);
+          barSpeed.innerHTML = veloPoke;
+
+          //habilidades
+          var habilidade1 = pokemon.abilities[0].ability.name;
+          hab1.innerHTML = habilidade1;
+
+          if (pokemon.abilities[1] != undefined || null) {
+            var habilidade2 = pokemon.abilities[1].ability.name;
+            hab2.innerHTML = habilidade2;
+          } else {
+            hab2.innerHTML = "none";
+          }
+        });
+
+      break;
+  }
 }
